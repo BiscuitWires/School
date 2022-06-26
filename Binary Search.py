133f9b0b06456aeba242251542d041eb57da5bfd
BiscuitWires <86314504+BiscuitWires@users.noreply.github.com>
2022-06-26 20:43:43 +0530

    Update Binary Search.py

--- Binary Search.py
+++ Binary Search.py
@@ -1,1 +1,19 @@
+arr = [1, 2, 3, 4, 5, 6]
+low = 0
+up = 5
+found = False
 
+target = int(input("Enter the target number "))
+
+while low <= up and not found:
+  mid = (low + up) // 2
+  if target == arr[mid]:
+    print ("Target found")
+    found = True
+  elif target > arr[mid]:
+    low = mid + 1
+  else:
+    up = mid - 1
+
+if not found:
+  print ("Target not found")
