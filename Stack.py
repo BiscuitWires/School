c9e08f37e4ec61f5bcfb54f3773d407589e42cec
BiscuitWires <86314504+BiscuitWires@users.noreply.github.com>
2022-07-11 13:53:00 +0530

    Update Stack.py

--- Stack.py
+++ Stack.py
@@ -2,26 +2,27 @@ array = [1, 2, 3, 4, 5]
 
 def push(arrName, element):
   global sp
-  sp = len(arrName) + 1
-  if sp == 4:
-    print ("stack is full")
+  sp = len(arrName) - 1
+  if sp == 5:
+    print ("Stack is full")
   else:
     sp = sp + 1
     arrName.append(element)
+    print ("Element added")
 
 def pop(arrName):
   global sp
   sp = len(arrName)
   if sp >= 0:
-    target = arrName[p - 1]
+    target = arrName[sp - 1]
     sp = sp - 1
     return target
 
-n1 = int(input("Enter first value"))
-n2 = int(input("Enter second value"))
+n1 = int(input("Enter first value "))
+n2 = int(input("Enter second value "))
 push(array, n1)
 push(array, n2)
 
 print("Array: ", array)
-print("Item popped was", pop(array))
-print(array[:p])
+print(pop(array), "was popped")
+print(array[:sp])
