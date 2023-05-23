a70ad376f47ced40d575d2dc97599d0a00ff570e
BiscuitWires <86314504+BiscuitWires@users.noreply.github.com>
2023-05-23 19:47:01 +0530

    Update Insertion Sort.py

--- Code Samples/Insertion Sort.py
+++ Code Samples/Insertion Sort.py
@@ -1,12 +1,12 @@
 arr = [6, 1, 5, 4, 2, 3, 3]
-current = 0
+key = 0
 temp = 0
 
 for i in range(1,7):
-  current = arr[i] 
+  key = arr[i] 
   j = i - 1
 
-  while j >= 0 and arr[j] > current:
+  while j >= 0 and arr[j] > key:
     temp = arr[j+1]
     arr[j+1] = arr[j]
     arr[j] = temp
